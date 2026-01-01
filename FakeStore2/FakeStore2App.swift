ebdd7e27750dd8ff0488c9967ace99869f80c9c3
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-01 18:45:29 +0100

    Initial Commit

---
+++ FakeStore2/FakeStore2App.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStore2App.swift
+//  FakeStore2
+//
+//  Created by Eric on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FakeStore2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
