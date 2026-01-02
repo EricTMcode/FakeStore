aec02eb4dcb5d52a344f38cc22cf7a86d171dfdd
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-02 16:41:51 +0100

    Implement caching logic with time-based refresh intervals

--- FakeStore2/Core/Service/ProductService.swift
+++ FakeStore2/Core/Service/ProductService.swift
@@ -14,9 +14,15 @@ protocol ProductServiceProtocol {
 struct ProductService: ProductServiceProtocol {
     private let URLString = "https://fakestoreapi.com/products"
     private let cache = ProductsCache()
+    private let refreshInterval: TimeInterval = 60 * 10 // 10 minutes
+    private var lastFetchedTime: Date?
+
+    init() {
+        getLastFetchedTime()
+    }
 
     func fetchProducts() async throws -> [Product] {
-        if let cachedProducts = try cache.getProducts() {
+        if !needsRefresh, let cachedProducts = try cache.getProducts() {
             print("DEBUG: Getting products from CACHE")
             return cachedProducts
         }
@@ -27,12 +33,22 @@ struct ProductService: ProductServiceProtocol {
         }
 
         let (data, response) = try await URLSession.shared.data(from: url)
-
         try validataResponse(response)
+
+        saveLastFetchedTime()
 
         let products = try JSONDecoder().decode([Product].self, from: data)
         cache.saveProducts(products)
+
         return products
+    }
+
+    private func saveLastFetchedTime() {
+        UserDefaults.standard.set(Date(), forKey: "lastFetchedTime")
+    }
+
+    private mutating func getLastFetchedTime() {
+        self.lastFetchedTime = UserDefaults.standard.value(forKey: "lastFetchedTime") as? Date
     }
 
     private func validataResponse(_ response: URLResponse) throws {
@@ -43,6 +59,13 @@ struct ProductService: ProductServiceProtocol {
         guard httpResponse.statusCode == 200 else {
             throw APIError.invalidResponse
         }
+    }
+
+    private var needsRefresh: Bool {
+        guard let lastFetchedTime else { return true }
+        print("DEBUG: Last fetched time \(lastFetchedTime)")
+        print("DEBUG: Time since \(Date().timeIntervalSince(lastFetchedTime))")
+        return Date().timeIntervalSince(lastFetchedTime) >= refreshInterval
     }
 }
 
