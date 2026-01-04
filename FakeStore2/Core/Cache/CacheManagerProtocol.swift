3447953a978a19b203a9272819ba4658f1f0c2a2
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-04 12:26:32 +0100

    Finalize project by cleaning line spacing

--- FakeStore2/Core/Cache/CacheManagerProtocol.swift
+++ FakeStore2/Core/Cache/CacheManagerProtocol.swift
@@ -11,7 +11,7 @@ protocol CacheManagerProtocol {
     func getData<T: Codable>(as type: T.Type) throws -> [T]
     func saveData<T: Codable>(_ data: [T])
     func invalidate()
-
+    
     var filename: String { get }
 }
 
@@ -19,7 +19,7 @@ extension CacheManagerProtocol {
     var documentDirectoryURL: URL? {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
     }
-
+    
     func getData<T: Codable>(as type: T.Type) throws -> [T] {
         guard let fileURL = documentDirectoryURL?.appending(path: filename) else {
             return []
@@ -28,12 +28,12 @@ extension CacheManagerProtocol {
         let data = try Data(contentsOf: fileURL)
         return try JSONDecoder().decode([T].self, from: data)
     }
-
+    
     func saveData<T: Codable>(_ data: [T]) {
         guard let fileURL = documentDirectoryURL?.appending(path: filename) else {
             return
         }
-
+        
         do {
             let data = try JSONEncoder().encode(data)
             try data.write(to: fileURL)
@@ -41,12 +41,12 @@ extension CacheManagerProtocol {
             print("Failed to save products to cache...")
         }
     }
-
+    
     func invalidate() {
         guard let fileURL = documentDirectoryURL?.appending(path: filename) else {
             return
         }
-
+        
         do {
             try FileManager.default.removeItem(at: fileURL)
         } catch {
