d43799c8b30ae2cd1eb7bdfe40957b4fa9df3531
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-04 11:30:09 +0100

    Add NavigationStack to UsersView and add User title on the page

--- FakeStore2/Core/View/UsersView.swift
+++ FakeStore2/Core/View/UsersView.swift
@@ -9,36 +9,39 @@ import SwiftUI
 
 struct UsersView: View {
     @State private var viewModel = UsersViewModel()
+    @State private var searchText = ""
 
     var body: some View {
-        VStack {
-            switch viewModel.loadingState {
-            case .loading:
-                ProgressView()
-            case .empty:
-                ContentUnavailableView("No Users Available", systemImage: "person.slash")
-            case .error(let error):
-                Text(error.localizedDescription)
-            case .completed(let users):
-                List {
-                    ForEach(users) { user in
-                        HStack(spacing: 16) {
-                            Text("\(user.id)")
+        NavigationStack {
+            VStack {
+                switch viewModel.loadingState {
+                case .loading:
+                    ProgressView()
+                case .empty:
+                    ContentUnavailableView("No Users Available", systemImage: "person.slash")
+                case .error(let error):
+                    Text(error.localizedDescription)
+                case .completed(let users):
+                    List {
+                        ForEach(users) { user in
+                            HStack(spacing: 16) {
+                                Text("\(user.id)")
 
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(user.username)
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(user.username)
 
-                                Text(user.email)
-                                    .foregroundStyle(.gray)
+                                    Text(user.email)
+                                        .foregroundStyle(.gray)
+                                }
                             }
+                            .font(.subheadline)
                         }
-                        .font(.subheadline)
                     }
                 }
-
             }
+            .navigationTitle("Users")
+            .task { await viewModel.fetchUsers() }
         }
-        .task { await viewModel.fetchUsers() }
     }
 }
 
